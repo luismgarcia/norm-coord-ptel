59870c0fefcc781fa3df4dfb525ddd15028e1387
Luis Muñoz <luismugor@gmail.com>
2025-11-23 23:37:21 +0100

    fix: corregir base path de /conversor-de-coorden/ a /norm-coord-ptel/

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/conversor-de-coorden/',
+  base: '/norm-coord-ptel/',
   plugins: [
     react(),
     tailwindcss(),
